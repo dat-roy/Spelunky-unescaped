948dbb5b3265dca33a12b9cfafab440051cececf
dat-roy <dat_r0y@outlook.com>
2022-05-12 21:55:29 +0700

    add texture clips class

---
+++ src/TextureClips.cpp
@@ -0,0 +1,34 @@
+#include "TextureClips.h"
+
+TextureClips::TextureClips()
+{
+    total_frames = 1;
+    current_frame = 0;
+}
+
+TextureClips::TextureClips(const int _total_frames) : total_frames(_total_frames)
+{
+//    total_frames = _total_frames;
+    current_frame = 0;
+}
+
+//Get total frames
+int TextureClips::getTotalFrames()
+{
+    return total_frames;
+}
+//Get current frame
+int TextureClips::getCurrentFrame()
+{
+    return current_frame;
+}
+//Go to next frame
+void TextureClips::nextFrame()
+{
+    current_frame++;
+}
+//Reset to the first frame
+void TextureClips::resetFrame()
+{
+    current_frame = 0;
+}
---
+++ src/TextureClips.h
@@ -0,0 +1,26 @@
+#ifndef TEXTURE_CLIPS_H__
+#define TEXTURE_CLIPS_H__
+
+#include <SDL.h>
+#include <iostream>
+#include <vector>
+
+struct TextureClips {
+    int total_frames;
+    int current_frame;
+    std::vector<SDL_Rect> clips;
+
+    TextureClips();
+    TextureClips(const int _total_frames);
+
+    //Get total frames
+    int getTotalFrames();
+    //Get current frame
+    int getCurrentFrame();
+    //Go to next frame
+    void nextFrame();
+    //Reset to the first frame
+    void resetFrame();
+};
+
+#endif // TEXTURE_CLIPS_H__
